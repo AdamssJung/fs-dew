a8e5feea2c28a5ccefef6e66953b111cdc0d913b
Minho Jung <wjdalsh87@gmail.com>
2025-07-20 00:24:46 +0900

    add database connection setup

---
+++ app/database.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv() # .env 에서 DATABASE_URL fhem
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bing=engine,
+)
