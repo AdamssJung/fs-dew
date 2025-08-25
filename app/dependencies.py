fadaa727763d4c9dab0d3a9248bca8894d363448
Minho Jung <wjdalsh87@gmail.com>
2025-08-25 22:54:13 +0900

    feat: add OAuth2PasswordBearer and get_current_user dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,35 @@
-from .database import SessionLocal
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from jose import JWTError
 from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import SessionLocal
+from .security import decode_token
 
-def get_db() -> Session:
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> models.User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate":"Bearer"},
+    )
+    try:
+        payload = decode_token(token)
+        username: str = payload.get("sub")
+        if not username:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = crud.get_user_by_username(db, username)
+    if not user:
+        raise credentials_exception
+    return user
